63234c8de28f5033c8ece8eca586ea28c01ad34b
Emmageo3 <kanfanyemma22@gmail.com>
2022-02-12 20:37:05 +0000

    configuration woo commerce

---
+++ wp-content/plugins/mailpoet/vendor/composer/installed.php
@@ -0,0 +1,45 @@
+<?php
+if (!defined('ABSPATH')) exit;
+ return array(
+ 'root' => array(
+ 'pretty_version' => 'dev-master',
+ 'version' => 'dev-master',
+ 'type' => 'library',
+ 'install_path' => __DIR__ . '/../../',
+ 'aliases' => array(),
+ 'reference' => '1fb1efd855d13d059554b9a5b5c9b7123448f614',
+ 'name' => '__root__',
+ 'dev' => false,
+ ),
+ 'versions' => array(
+ '__root__' => array(
+ 'pretty_version' => 'dev-master',
+ 'version' => 'dev-master',
+ 'type' => 'library',
+ 'install_path' => __DIR__ . '/../../',
+ 'aliases' => array(),
+ 'reference' => '1fb1efd855d13d059554b9a5b5c9b7123448f614',
+ 'dev_requirement' => false,
+ ),
+ 'mtdowling/cron-expression' => array(
+ 'pretty_version' => 'v1.2.3',
+ 'version' => '1.2.3.0',
+ 'type' => 'library',
+ 'install_path' => __DIR__ . '/../mtdowling/cron-expression',
+ 'aliases' => array(),
+ 'reference' => '9be552eebcc1ceec9776378f7dcc085246cacca6',
+ 'dev_requirement' => false,
+ ),
+ 'soundasleep/html2text' => array(
+ 'pretty_version' => 'dev-master',
+ 'version' => 'dev-master',
+ 'type' => 'library',
+ 'install_path' => __DIR__ . '/../soundasleep/html2text',
+ 'aliases' => array(
+ 0 => '9999999-dev',
+ ),
+ 'reference' => 'd907c8fc20605135b4ac29b7a2f43d8c1c6cddb8',
+ 'dev_requirement' => false,
+ ),
+ ),
+);
